264003567640f14d4d3ffb6776998c349caadf44
Cillian O'Ruanaidh <contact@cilliano.com>
2022-06-17 11:28:53 +0100

    Undo changes to tests, now that order cycle returns all shipping methods by default and doesn't explicitly require OrderCycleShippingMethods anymore

--- spec/system/consumer/split_checkout_spec.rb
+++ spec/system/consumer/split_checkout_spec.rb
@@ -68,15 +68,7 @@ describe "As a consumer, I want to checkout my order", js: true do
     add_enterprise_fee enterprise_fee
     set_order order
 
-    shipping_methods = [
-      free_shipping_with_required_address,
-      free_shipping,
-      shipping_with_fee,
-      free_shipping_without_required_address,
-      tagged_shipping
-    ]
-    distributor.shipping_methods << shipping_methods
-    order_cycle.shipping_methods << shipping_methods
+    distributor.shipping_methods.push(free_shipping_with_required_address, free_shipping, shipping_with_fee, free_shipping_without_required_address, tagged_shipping)
   end
 
   context "guest checkout when distributor doesn't allow guest orders" do
